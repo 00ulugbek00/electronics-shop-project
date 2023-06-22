fb35266352ba0d55166a33df224f482a3350a65a
Ulugbek Nishanov <daynnishanov@gmail.com>
2023-06-22 20:32:26 +0700

    функции add

--- src/item.py
+++ src/item.py
@@ -1,27 +1,66 @@
 class Item:
-    '''
-    Класс для предоставления товара в магазине
-    '''
     pay_rate = 1.0
     all = []
 
-    def __init__(self, product_name: str, price, quantity: int):
+    def __init__(self, name: str, price, quantity: int) -> None:
         '''
          Создание экземпляров Item
         '''
-        self.product_name = product_name
+        self.name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
 
-    def calculate_total_price(self):
+    @property
+    def name(self) -> str:
+        """
+        Получает название товара
+        """
+        return self.__name
+
+    @name.setter
+    def name(self, value) -> None:
+        """
+        Устанавливает название товара.
+        Если длина названия превышает 10 символов, обрезает его до первых 10 символов.
+        :param value: Новое название товара.
+        """
+        self.__name = value if len(value) <= 10 else value[:10]
+
+    def calculate_total_price(self) -> float:
         '''
         Возвращение общей стоимости
         '''
         return self.price * self.quantity
 
-    def apply_discount(self):
-        '''
-        Принимает скидку
-        '''
+    def apply_discount(self) -> None:
+        """
+        Применяет установленную скидку для конкретного товара.
+        """
         self.price *= self.pay_rate
+
+    def private_name(self):
+        print(self.name)
+
+    @staticmethod
+    def string_to_number(value: str) -> int:
+        """
+        Преобразует числовую строку в число.
+
+        :param value: Числовая строка.
+        :return: Преобразованное число.
+        """
+        return int(float(value))
+
+    @classmethod
+    def instantiate_from_csv(cls) -> None:
+        """
+        Инициализирует экземпляры класса Item данными из CSV-файла.
+
+        Файл должен содержать строки с данными в формате: "name,price,quantity".
+        :raises: FileNotFoundError, если файл items.csv не найден.
+        :raises: InstantiateCSVError, если файл item.csv поврежден.
+     """
+
+
+
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,39 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+import pytest
+
+def test_item_name(item1, item2):
+    item1.name = 'Смартфон'
+    item2.name = 'Смартфон'
+    assert item1.name == 'Смартфон'
+
+
+def test_item1_price(item1):
+    assert item1.price == 1000.0
+
+
+def test_item1_quantity(item1):
+    assert item1.quantity == 5
+
+
+def test_item1_total_price(item1):
+    assert item1.calculate_total_price() == 5000.0
+
+
+def test_item1_discount(item1):
+    item1.pay_rate = 0.9
+    item1.apply_discount()
+    assert item1.price == 900.0
+
+
+def test_item1_from_csv_name():
+    Item.instantiate_from_csv()
+    item1 = Item.all[0]
+    assert item1.name == 'Смартфон'
+
+
+def test_item1(item2):
+    assert item2 == "Item('Смартфон', 10000, 20)"
+
+
+def test_item1_str(item2):
+    assert str(item2) == 'Смартфон'
