ad8bbf9da3c88e3a452f36840a2d5aec42857a5a
Ulugbek Nishanov <daynnishanov@gmail.com>
2023-07-15 19:56:47 +0700

    Добавьте в метод instantiate_from_csv() класса Item обработку исключений:
    если файл items.csv, из которого по умолчанию считываются данные, не найден → выбрасывается исключение FileNotFoundError с сообщением “Отсутствует файл item.csv"
    если файл item.csv поврежден (например, отсутствует одна из колонок данных) → выбрасывается исключение InstantiateCSVError с сообщением “Файл item.csv поврежден”.

--- src/item.py
+++ src/item.py
@@ -1,6 +1,10 @@
 import csv
 
 
+class InstantiateCSVError:
+    pass
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -10,51 +14,31 @@ class Item:
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
-        экземпляра класса item.
+        Создание экземпляра класса item.
+
+        :param name: Название товара.
+        :param price: Цена за единицу товара.
+        :param quantity: Количество товара в магазине.
         """
         self.__name = name
-        self.__price = price
-        self.__quantity = quantity
+        self.price = price
+        self.quantity = quantity
         self.all.append(self)
 
     def __repr__(self):
-        return f'{self.__class__.__name__},{self.name},{self.price},{self.__quantity}'
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
 
     def __str__(self):
-        return self.__name
+        return self.name
 
     @property
     def name(self) -> str:
-        """
-        Название товара.
-        """
         return self.__name
 
     @name.setter
-    def name(self, value) -> None:
-        """
-        Устанавливает название товара.
-        Если длина названия превышает 10 символов, обрезает его до первых 10 символов.
-        :param value: Новое название товара.
-        """
-        self.__name = value if len(value) <= 10 else value[:10]
-
-    @property
-    def price(self) -> float:
-        """
-        Получает количество товара в магазине.
-
-        """
-        return self.__price
-
-    @property
-    def quantity(self) -> int:
-        """
-        Получает цену за единицу товара.
-
-        :return: Цена за единицу товара.
-        """
-        return self.__quantity
+    def name(self, value: str):
+        if len(value) <= 10:
+            self.__name = value
 
     def calculate_total_price(self) -> float:
         """
@@ -62,28 +46,33 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        return self.price * self.__quantity
+        return self.price * self.quantity
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        self.__price *= self.pay_rate
+        self.price *= self.pay_rate
 
     @staticmethod
-    def string_to_number(value: str) -> int:
-        """
-        Преобразует числовую строку в число.
-
-        :param value: Числовая строка.
-        :return: Преобразованное число.
-        """
-        return int(float(value))
+    def string_to_number(value: str):
+        return int(value)
 
     @classmethod
-    def instantiate_from_csv(cls):
-        cls.all.clear()
-        with open('../src/items.csv', 'r', encoding='windows-1251') as file:
-            file_reader = csv.DictReader(file)
-            for row in file_reader:
-                cls(row['name'], float(row['price']), int(row['quantity']))
+    def instantiate_from_csv(cls, filename) -> str:
+        """
+        класс-метод, инициализирующий экземпляры класса `Item` данными из файла _src/items.csv_
+        """
+        try:
+            with open(filename) as f:
+                reader = csv.DictReader(f)
+                cls.all.clear()
+                for row in reader:
+                    name = row['name']
+                    price = cls.string_to_number(row['price'])
+                    quantity = cls.string_to_number(row['quantity'])
+                    cls.all.append(cls(name, price, quantity))  # создаем экзмляры классы и кладем их в список
+        except FileNotFoundError:
+            return f"Отсутствует файл {filename}"
+        except InstantiateCSVError:
+            return f"Файл {filename} поврежден"
